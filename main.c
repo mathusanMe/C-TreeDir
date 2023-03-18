63721ae0538a66ff1fb716be047263d0b5307282
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-03-18 14:36:30 +0100

    Update import from main

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include "utils/nodes.h"
+#include "commands/print/print.c"
 
 int main()
 {
+    noeud root_node = {true, "root", NULL, NULL, NULL};
+    print_node(root_node);
     return EXIT_SUCCESS;
 }
