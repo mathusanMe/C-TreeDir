9f76057e86f0e22ef3e3e151d2c8b7c114e813b7
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-03-20 11:15:49 +0100

    Add documentation

--- commands/pwd/pwd.c
+++ commands/pwd/pwd.c
@@ -1,11 +1,20 @@
 #include "pwd.h"
 
+/**
+ * Print the path of the working directory
+ * Then print newline
+ * @param node current node
+ */
 void pwd(noeud *node)
 {
     pwd_rec(node);
     puts("");
 }
 
+/**
+ * By walking back through the entire file tree,
+ * from the root node, print the file's name then it's child's one
+ */
 void pwd_rec(noeud *node)
 {
     if (node->pere == node)
