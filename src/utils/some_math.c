eadf342b95af0393039ea7fbae66a1a994095fa9
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-04-06 13:15:05 +0200

    Wrote again min and max functions

---
+++ src/utils/some_math.c
@@ -0,0 +1,11 @@
+#include "some_math.h"
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
---
+++ src/utils/some_math.h
@@ -0,0 +1,7 @@
+#ifndef SOME_MATH_H
+#define SOME_MATH_H
+
+int min(int a, int b);
+int max(int a, int b);
+
+#endif
