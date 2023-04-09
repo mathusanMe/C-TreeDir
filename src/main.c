5999fd2231889d4c3dd7812489202f6ca90a3591
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-04-09 13:43:06 +0200

    Bool to int

--- src/main.c
+++ src/main.c
@@ -8,7 +8,11 @@ int main(int argc, char *argv[])
 {
     if (argc == 2 && strcmp(argv[1], "test") == 0)
     {
-        return run_tests();
+        if (run_tests())
+        {
+            return EXIT_SUCCESS;
+        }
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
