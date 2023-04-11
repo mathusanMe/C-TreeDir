f3ba4b025a08841a050b9718d5da120ef80f1794
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-04-11 15:42:47 +0200

    Remove example

--- tests/main_test.c
+++ tests/main_test.c
@@ -9,8 +9,5 @@ bool run_tests()
 
     bool all_tests_passed = true;
 
-    char *segmentation_fault = malloc(3 * sizeof(char));
-    memmove(segmentation_fault, "ezrg", 3 * sizeof(char));
-
     return all_tests_passed;
 }
