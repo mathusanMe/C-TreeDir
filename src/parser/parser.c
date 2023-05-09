2f40cf824bde0e84ac036432fed2bd68e7f6deda
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-05-09 17:12:36 +0200

    Parse line

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -28,3 +28,62 @@ void parse_file(char *file_path)
 
     exit(EXIT_SUCCESS);
 }
+
+void parse_line(char *line)
+{
+    char *strToken = strtok(line, SEPARATORS);
+
+    if (strcmp(strToken, "ls") == 0)
+    {
+        // TODO: Check that next token is null
+        // TODO: Send toward ls command with no arguments
+    }
+
+    if (strcmp(strToken, "cd") == 0)
+    {
+        // TODO: Disintguish cases
+        // TODO: Send toward cd command with associated arguments
+    }
+
+    if (strcmp(strToken, "pwd") == 0)
+    {
+        // TODO: Check that next token is null
+        // TODO: Send toward pwd command with no arguments
+    }
+
+    if (strcmp(strToken, "mkdir") == 0)
+    {
+        // TODO: Check that next token is not null and the next is null
+        // TODO: Send toward mkdir command with one arguments
+    }
+
+    if (strcmp(strToken, "touch") == 0)
+    {
+        // TODO: Check that next token is not null and the next is null
+        // TODO: Send toward touch command with one arguments
+    }
+
+    if (strcmp(strToken, "rm") == 0)
+    {
+        // TODO: Check that next token is not null and the next is null
+        // TODO: Send toward rm command with one arguments
+    }
+
+    if (strcmp(strToken, "cp") == 0)
+    {
+        // TODO: Check that next two tokens are not null and that the third one is
+        // TODO: Send toward cp command with two arguments
+    }
+
+    if (strcmp(strToken, "mv") == 0)
+    {
+        // TODO: Check that next two tokens are not null and that the third one is
+        // TODO: Send toward mv command with two arguments
+    }
+
+    if (strcmp(strToken, "print") == 0)
+    {
+        // TODO: Check that next token is null
+        // TODO: Send toward print command with no arguments
+    }
+}
