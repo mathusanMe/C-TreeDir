9f7ea1fc4d8fb93a449b652b78eb67da01bde399
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2023-05-13 17:59:26 +0200

    bugfixed

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "utils/nodes.h"
 #include "../tests/main_test.h"
 
 int main(int argc, char *argv[])
--- tests/main_test.h
+++ tests/main_test.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "../src/utils/math.h"
-#include "commands/mkdir/mkdir.h"
+#include "commands/mkdir/test_mkdir.h"
 
 bool run_tests();
 
