f805567a1ef76fc6e0e265ad06602f2f164f505a
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2023-05-13 15:31:39 +0200

    fixed error

--- tests/main_test.h
+++ tests/main_test.h
@@ -3,8 +3,6 @@
 
 #include <stdlib.h>
 #include "commands/test/test.h"
-#include "commands/another_command/another_command.h"
-#include "commands/wc/test_wc.h"
 #include "../src/utils/math.h"
 
 bool run_tests();
