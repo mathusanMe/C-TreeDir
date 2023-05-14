d75afd163dc0e4dc4ae04c17ecee24fef1f7523e
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-05-14 12:58:04 +0200

    Send main args into parser

--- src/main.c
+++ src/main.c
@@ -1,12 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include "utils/string.h"
+#include "parser/parser.h"
 #include "utils/nodes.h"
 #include "../tests/main_test.h"
 
+#define TESTS_KEYS                       \
+    {                                    \
+        "test", "tests", "TEST", "TESTS" \
+    }
+
+#define TESTS_KEYS_SIZE 4
+
 int main(int argc, char *argv[])
 {
-    if (argc == 2 && strcmp(argv[1], "test") == 0)
+    if (argc != 2)
+    {
+        return EXIT_FAILURE;
+    }
+
+    char **tests_keys = TESTS_KEYS;
+
+    if (contains(tests_keys, TESTS_KEYS_SIZE, argv[1]))
     {
         if (run_tests())
         {
@@ -14,6 +29,8 @@ int main(int argc, char *argv[])
         }
         return EXIT_FAILURE;
     }
+
+    parse_file(argv[1]);
 
     return EXIT_SUCCESS;
 }
