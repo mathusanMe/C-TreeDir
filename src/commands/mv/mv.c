579da6876480b763eb3b7e89f2d5bc5b49601446
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-05-18 17:43:45 +0200

    Structure of mv files

---
+++ src/commands/mv/mv.c
@@ -0,0 +1,5 @@
+#include "mv.h"
+
+bool mv(noeud *current, char *src, char *dest)
+{
+}
---
+++ src/commands/mv/mv.h
@@ -0,0 +1,8 @@
+#ifndef MV_H
+#define MV_H
+
+#include "../utils/nodes.h"
+
+bool mv(noeud *current, char *src, char *dest);
+
+#endif
