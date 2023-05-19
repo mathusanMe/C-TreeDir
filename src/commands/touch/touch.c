7784580a4af7cf70ca4d82f3107fcf222363e565
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2023-05-19 05:59:07 +0200

    Add VERBOSE mode to touch

--- src/commands/touch/touch.c
+++ src/commands/touch/touch.c
@@ -9,7 +9,10 @@ bool touch(noeud *current, char *name)
 
     if (!current->est_dossier)
     {
-        printf("touch: %s is not a folder.\n", current->nom);
+        if (VERBOSE)
+        {
+            printf("touch: %s is not a folder.\n", current->nom);
+        }
         return false;
     }
 
@@ -20,7 +23,10 @@ bool touch(noeud *current, char *name)
         current->fils = malloc(sizeof(liste_noeud));
         if (current->fils == NULL)
         {
-            printf("touch: failed to allocate memory.\n");
+            if (VERBOSE)
+            {
+                printf("touch: failed to allocate memory.\n");
+            }
             return false;
         }
         current->fils->no = create_node(name, false, current, current->racine);
@@ -34,22 +40,28 @@ bool touch(noeud *current, char *name)
     {
         if (strcmp(children->no->nom, name) == 0)
         {
-            printf("touch: file already exists.\n");
-            return false;
+            printf("touch: file already exists. exit program.\n");
+            exit(1);
         }
     }
 
     noeud *new_node = create_node(name, false, current, current->racine);
     if (new_node == NULL)
     {
-        printf("touch: failed to create node.\n");
+        if (VERBOSE)
+        {
+            printf("touch: failed to create node.\n");
+        }
         return false;
     }
 
     last_child->succ = create_list_node(new_node, NULL);
     if (last_child->succ == NULL)
     {
-        printf("touch: failed to create list node.\n");
+        if (VERBOSE)
+        {
+            printf("touch: failed to create list node.\n");
+        }
         free(new_node);
         return false;
     }
